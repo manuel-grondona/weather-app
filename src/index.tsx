b663bac466b29e5cdde5f902f653d8d392a2bf38
manuel-grondona <mgrondona@gmail.com>
2021-04-21 20:37:21 -0300

    Add global style

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react"
+import ReactDOM from "react-dom"
+import App from "./App"
+import reportWebVitals from "./reportWebVitals"
+import { createGlobalStyle } from "styled-components"
+
+const GlobalStyle = createGlobalStyle`
+  :root {
+    box-sizing: border-box
+  };
+
+  body {
+    margin: 0;
+  }
+
+  *, ::before, ::after {
+    box-sizing: inherit
+  }
+`
 
 ReactDOM.render(
   <React.StrictMode>
+    <GlobalStyle />
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
-);
+  document.getElementById("root")
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
