518dd015cea32ff1ab9285347f786e4387baedde
manuel-grondona <mgrondona@gmail.com>
2021-04-26 21:37:04 -0300

    Add Arrow icon

---
+++ src/icons/Arrow.tsx
@@ -0,0 +1,19 @@
+import * as React from "react"
+
+export function Arrow(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      width={40}
+      height={40}
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path
+        d="M12.293 5.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L16.586 13H5a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+        fill="#0D0D0D"
+      />
+    </svg>
+  )
+}
